17d86db087381a8b369a619ec9f5fcbaa1b9f38e
rofinn <rory.finnegan@gmail.com>
2020-12-12 17:51:30 -0600

    Update documentation deps and fix doctests for dataframes 0.22

--- src/validators.jl
+++ src/validators.jl
@@ -74,16 +74,17 @@ the minimum internal `_validate` call requirements.
 ```jldoctest
 julia> using DataFrames, Test; using Impute: Threshold, ThresholdError, validate
 
+
 julia> df = DataFrame(:a => [1.0, 2.0, missing, missing, 5.0], :b => [1.1, 2.2, 3.3, missing, 5.5])
 5×2 DataFrame
-│ Row │ a        │ b        │
-│     │ Float64? │ Float64? │
-├─────┼──────────┼──────────┤
-│ 1   │ 1.0      │ 1.1      │
-│ 2   │ 2.0      │ 2.2      │
-│ 3   │ missing  │ 3.3      │
-│ 4   │ missing  │ missing  │
-│ 5   │ 5.0      │ 5.5      │
+ Row │ a          b
+     │ Float64?   Float64?
+─────┼──────────────────────
+   1 │       1.0        1.1
+   2 │       2.0        2.2
+   3 │ missing          3.3
+   4 │ missing    missing
+   5 │       5.0        5.5
 
 julia> @test_throws ThresholdError validate(df, Threshold())
 Test Passed
